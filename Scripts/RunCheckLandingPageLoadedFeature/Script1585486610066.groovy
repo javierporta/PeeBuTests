0acbb2e42183358b2ad2946e24c69d00968e747e
Javier <2190271@my.ipleiria.pt>
2020-04-01 15:40:12 +0100

    Include disabled run feature folder step

--- Scripts/RunCheckLandingPageLoadedFeature/Script1585486610066.groovy
+++ Scripts/RunCheckLandingPageLoadedFeature/Script1585486610066.groovy
@@ -17,3 +17,5 @@ import internal.GlobalVariable as GlobalVariable
 
 CucumberKW.runFeatureFile('Include/features/CheckLandingPageLoaded.feature')
 
+not_run: CucumberKW.runFeatureFolder('Include/features')
+
