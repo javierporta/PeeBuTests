b88d4ea838505080dfe60001b53844e10a00f3ad
Javier <2190271@my.ipleiria.pt>
2020-04-27 14:17:28 +0100

    Add CheckTransactionGridFilteringStepDef empty file

---
+++ Include/scripts/groovy/stepdefs/peebu/CheckTransactionsGridFilteringStepDef.groovy
@@ -0,0 +1,128 @@
+package stepdefs.peebu
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+
+import cucumber.api.java.en.And
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+
+
+
+class CheckTransactionsGridFilteringStepDef {
+	/**
+	 * The step definitions below match with Katalon sample Gherkin steps
+	 */
+	@When("I click transactionId filter")
+public void i_click_transactionId_filter() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I type some {int} to filter")
+public void i_type_some_to_filter(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I click filter button")
+public void i_click_filter_button() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@Then("I see only filtered transactions")
+public void i_see_only_filtered_transactions() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I click entity filter")
+public void i_click_entity_filter() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I type some Hilll Group to filter")
+public void i_type_some_Hilll_Group_to_filter() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I click type filter")
+public void i_click_type_filter() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I type some withdrawal to filter")
+public void i_type_some_withdrawal_to_filter() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I click source filter")
+public void i_click_source_filter() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I type some source {int} to filter")
+public void i_type_some_source_to_filter(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I click {string} filter")
+public void i_click_filter(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I select a {string} to filter")
+public void i_select_a_to_filter(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("I write a {string} to filter")
+public void i_write_a_to_filter(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+}
